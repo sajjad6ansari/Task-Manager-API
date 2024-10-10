82987526a769c0675a3596c5b945fcb32aeba406
sajjad6ansari <sajjad6ansari@gmail.com>
2024-10-10 12:02:36 +0530

    completed all apis

--- app.js
+++ app.js
@@ -1,35 +1,34 @@
-const express=require('express')
-const app=express()
-const router=require('./routes/tasks')
-const connectDB=require('./db/connect')
-require('dotenv').config()
-const notFound=require('./middlewares/not-found')
-const errorHandlerMiddleware = require('./middlewares/error-handler')
+const express = require("express")
+const app = express()
+const router = require("./routes/tasks")
+const connectDB = require("./db/connect")
+require("dotenv").config()
+const notFound = require("./middlewares/not-found")
+const errorHandlerMiddleware = require("./middlewares/error-handler")
 
 //middleware
-app.use(express.static('./public'))
+app.use(express.static("./public"))
 app.use(express.json())
 
 // routes
-app.use('/api/v1/tasks',router)
+app.use("/api/v1/tasks", router)
 
 app.use(notFound)
 app.use(errorHandlerMiddleware)
 
-const port=process.env.PORT ||3000
+const port = process.env.PORT || 3000
 
-const start=async ()=>{
-    try {
-        await connectDB(process.env.MONGO_URI)
-        // console.log('connected to DB')
-        app.listen(port,console.log(`server listening on the port ${port}....`))
-    } catch (error) {
-        console.log(error)
-    }
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    // console.log('connected to DB')
+    app.listen(port, console.log(`server listening on the port ${port}....`))
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 start()
- 
 
 //app.get('/api/v1/tasks')-                 get all tasks
 //app.post('/api/v1/tasks')-                post a  task
